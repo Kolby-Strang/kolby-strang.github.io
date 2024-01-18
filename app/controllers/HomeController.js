a8591a7b2f6fa659a8ed82a714b0ba7e1507bd7e
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2024-01-18 11:58:08 -0700

    LOD Loading for hero image

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -12,4 +12,17 @@ export class HomeController {
       return
     }
   }
+  upgradeImgQuality(hiElement) {
+    console.log('hi');
+    const hiElemId = hiElement.id
+    const loElemId = 'lo' + hiElemId.substring(2)
+    console.log(loElemId);
+    const loElement = document.getElementById(loElemId)
+    if (loElement) {
+      loElement.classList.add('hidden')
+      hiElement.classList.remove('hidden')
+      return
+    }
+    console.log('no');
+  }
 }
