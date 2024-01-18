9ece136d322ef8516a3aac60b925c3cb046dcf8c
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2024-01-17 22:22:20 -0700

    WOOOOO

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -2,6 +2,17 @@ import { Pop } from "../utils/Pop.js"
 
 // Public
 export class HomeController {
+  constructor() {
+    const sections = document.querySelectorAll("section");
+
+    [...sections].forEach((section) => {
+      const checkbox = section.querySelector("input");
+
+      checkbox.addEventListener("change", () => {
+        section.classList.toggle("enable-animation");
+      });
+    });
+  }
   scrollToElem(elemId) {
     const element = document.getElementById(elemId)
     console.log(element)
