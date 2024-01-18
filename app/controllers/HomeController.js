c48009a2881805649f3ce7c7e71b165fc84518a6
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2024-01-18 11:44:33 -0700

    ScrollToFix

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -3,19 +3,13 @@ import { Pop } from "../utils/Pop.js"
 // Public
 export class HomeController {
   constructor() {
-    const sections = document.querySelectorAll("section");
-
-    [...sections].forEach((section) => {
-      const checkbox = section.querySelector("input");
-
-      checkbox.addEventListener("change", () => {
-        section.classList.toggle("enable-animation");
-      });
-    });
+    return
   }
   scrollToElem(elemId) {
     const element = document.getElementById(elemId)
-    console.log(element)
-    element.scrollIntoView()
+    if (element) {
+      element.scrollIntoView()
+      return
+    }
   }
 }
