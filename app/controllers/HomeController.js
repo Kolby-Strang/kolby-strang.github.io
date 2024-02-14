1ef23a676ef5907c136e3e58567b0d5b75bb5587
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2024-02-14 13:50:07 -0700

    Updated Resume

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,10 +1,4 @@
-import { Pop } from "../utils/Pop.js"
-
-// Public
 export class HomeController {
-  constructor() {
-    return
-  }
   scrollToElem(elemId) {
     const element = document.getElementById(elemId)
     if (element) {
@@ -13,16 +7,13 @@ export class HomeController {
     }
   }
   upgradeImgQuality(hiElement) {
-    console.log('hi');
     const hiElemId = hiElement.id
     const loElemId = 'lo' + hiElemId.substring(2)
-    console.log(loElemId);
     const loElement = document.getElementById(loElemId)
     if (loElement) {
       loElement.classList.add('hidden')
       hiElement.classList.remove('hidden')
       return
     }
-    console.log('no');
   }
 }
--- app/router-config.js
+++ app/router-config.js
@@ -1,4 +1,3 @@
-import { ExamplesController } from "./controllers/ExamplesController.js";
 import { HomeController } from "./controllers/HomeController.js";
 import { Router } from "./utils/Router.js";
 
@@ -6,7 +5,7 @@ import { Router } from "./utils/Router.js";
 export const router = new Router([
   {
     path: '',
-    controllers: [HomeController, ExamplesController],
+    controllers: [HomeController],
     view: 'app/views/HomeView.html'
   }
 ])
